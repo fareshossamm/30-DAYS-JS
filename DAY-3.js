fc0729689bbc96bfdaebe6cec03980114db3f3ea
Fares Hossam <fareshossam201@gmail.com>
2024-07-11 01:31:05 +0300

    put each problem text in comment

--- DAY-3.js
+++ DAY-3.js
@@ -1,7 +1,9 @@
-/*
-            closures
-            problem three -- To Be Or Not To Be  --  
-*/
+
+/* 
+    closures
+     problem two -- To Be Or Not To Be --  
+ */
+
 var expect = function (val) {
   return {
     toBe: function (value) {
@@ -24,3 +26,28 @@ var expect = function (val) {
 console.log(expect(5).toBe(5)); // "value: true"
 console.log(expect(5).toBe(null)); // "error: Not Equal"
 console.log(expect(5).notToBe(null)); // "value: true"
+
+
+/*
+Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+
+toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
+
+
+Example 1:
+
+Input: func = () => expect(5).toBe(5)
+Output: {"value": true}
+Explanation: 5 === 5 so this expression returns true.
+Example 2:
+
+Input: func = () => expect(5).toBe(null)
+Output: {"error": "Not Equal"}
+Explanation: 5 !== null so this expression throw the error "Not Equal".
+Example 3:
+
+Input: func = () => expect(5).notToBe(null)
+Output: {"value": true}
+Explanation: 5 !== null so this expression returns true.
+*/
